50859f0143479c7331b27827e0fe433f7048b4d1
Adam Hitchens <arhitchens@users.noreply.github.com>
2020-03-28 17:56:35 -0500

    Adding bike service and admin component

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeService } from './services/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, AdminComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
